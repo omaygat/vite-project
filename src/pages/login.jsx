f870d87d3b63c3e38db6d166f226ab569edfb7bc
omaygat <mallmita720@gmail.com>
2025-05-20 16:48:20 -0500

    Ajuste de redirección y configuración de puerto en Vite

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -45,3 +45,4 @@ function Login() {
 
 export default Login;
 
+
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
+})
