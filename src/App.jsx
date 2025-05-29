2971ace6326399a8296df25d3733db50a6043e1c
omaygat <mallmita720@gmail.com>
2025-05-29 12:38:09 -0500

    actualizacion de app

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function NavBar() {
           <Link to="/ventas" style={{ marginRight: "15px" }}>Ventas</Link>
           <Link to="/registrar-venta" style={{ marginRight: "15px" }}>Registrar Venta</Link>
           <Link to="/prediccion" style={{ marginRight: "15px" }}>Predicción de Ventas</Link>
+          <Link to="/prediccion" style={{ marginRight: "15px" }}>Predicción de Ventas</Link>
         </>
       ) : (
         <>
